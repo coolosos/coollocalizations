9e4fadf3a5d96f16a430fc20abf9e450a3294cb2
Cayetano Bañón <cayetano.banon@gmail.com>
2024-02-05 10:33:43 +0100

    Change export

--- lib/coollocalizations.dart
+++ lib/coollocalizations.dart
@@ -4,4 +4,3 @@ export 'multi_choice_localizations.dart';
 export 'multi_choice_replacements_localizations.dart';
 export 'replacements_localizations.dart';
 export 'extension/replace_extension.dart';
-export 'package:json_annotation/json_annotation.dart';
